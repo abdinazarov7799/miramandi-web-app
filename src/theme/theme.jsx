cdfd39a37a7e0a27ea34473130f8488a2e9979e8
Diyorbek Abdinazarov <109986151+abdinazarov7799@users.noreply.github.com>
2024-09-10 19:32:16 +0500

    Update theme.jsx

--- src/theme/theme.jsx
+++ src/theme/theme.jsx
@@ -41,7 +41,7 @@ const GlobalStyles = createGlobalStyle`
         -moz-appearance: textfield;
     }
     .active button {
-        background-color: #EC1B24;
+        background-color: #1bda15;
         color: #fff;
         font-weight: 500;
     }
